394f30f8da32066218f88cb1296368f44eadfb70
rahmann5 <rahmann5@aston.ac.uk>
2018-08-27 12:26:25 +0100

    Test: Added services and chat activity as well as related layouts

--- app/src/main/java/com/example/naziur/androidchat/Activities/MainActivity.java
+++ app/src/main/java/com/example/naziur/androidchat/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.naziur.androidchat;
+package com.example.naziur.androidchat.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.naziur.androidchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
