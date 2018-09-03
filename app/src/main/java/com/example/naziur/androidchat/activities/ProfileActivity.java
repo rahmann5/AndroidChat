9772d8092c835b70fe318e631d1b475c3bd2637a
hsrahman <rahmanh4@aston.ac.uk>
2018-09-03 14:31:00 +0100

    In progress profile activity

--- app/src/main/java/com/example/naziur/androidchat/activities/ProfileActivity.java
+++ app/src/main/java/com/example/naziur/androidchat/activities/ProfileActivity.java
@@ -3,19 +3,98 @@ package com.example.naziur.androidchat.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.naziur.androidchat.R;
+import com.example.naziur.androidchat.utils.FadingActionBarHelper;
 import com.example.naziur.androidchat.models.User;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
     User user = User.getInstance();
+    private ListView myGroups, myContacts;
+    private TextView emptyGroupsList, emptyContactsList;
+    private AppCompatButton saveButton;
+    private ImageView editToggle, prodilePic;
+    private ImageButton updatePic;
+    private AppCompatEditText profileName;
+    private Spinner profileStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
         setTitle(user.profileName);
+        FadingActionBarHelper helper = new FadingActionBarHelper()
+                .actionBarBackground(R.color.colorPrimaryDark)
+                .headerLayout(R.layout.header)
+                .contentLayout(R.layout.activity_profile)
+                .allowHeaderTouchEvents(true);
+        setContentView(helper.createView(this));
+        helper.initActionBar(this);
+
+        prodilePic = (ImageView) findViewById(R.id.image_header);
+        updatePic = (ImageButton) findViewById(R.id.update_profile_pic);
+        updatePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ProfileActivity.this, "Updating profile picture", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        profileStatus = (Spinner) findViewById(R.id.prof_status);
+
+        profileName = (AppCompatEditText) findViewById(R.id.edit_prof_name);
+        profileName.setText(user.profileName);
+        editToggle = (ImageView) findViewById(R.id.edit_button_prof_name);
+
+        saveButton = (AppCompatButton) findViewById(R.id.save_profile_btn);
+        ArrayAdapter<String> adapterContacts = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        emptyContactsList = (TextView) findViewById(R.id.no_contacts);
+        myContacts = (ListView) findViewById(R.id.profile_contacts_list);
+        myContacts.setAdapter(adapterContacts);
+        myContacts.setEmptyView(emptyContactsList);
+
+        emptyGroupsList = (TextView) findViewById(R.id.no_groups);
+        ArrayAdapter<String> adapterGroup = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        myGroups = (ListView) findViewById(R.id.profile_groups_list);
+        myGroups.setAdapter(adapterGroup);
+        myGroups.setEmptyView(emptyGroupsList);
+
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!profileName.isEnabled()) {
+                    profileName.setEnabled(true);
+                } else {
+                    profileName.setEnabled(false);
+                }
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ProfileActivity.this, "Saving changes", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 
 
---
+++ app/src/main/java/com/example/naziur/androidchat/utils/FadingActionBarHelper.java
@@ -0,0 +1,49 @@
+package com.example.naziur.androidchat.utils;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Hamidur on 03/09/2018.
+ */
+
+public class FadingActionBarHelper extends FadingActionBarHelperBase {
+
+
+    private ActionBar mActionBar;
+
+    @Override
+    public void initActionBar(Activity activity) {
+        mActionBar = getActionBar(activity);
+        super.initActionBar(activity);
+    }
+
+    private ActionBar getActionBar(Activity activity) {
+        if (activity instanceof ActionBarActivity) {
+            return ((ActionBarActivity) activity).getSupportActionBar();
+        }
+        ActionBar actionBar = getActionBarWithReflection(activity, "getSupportActionBar");
+        if (actionBar == null) {
+            throw new RuntimeException("Activity should derive from ActionBarActivity "
+                    + "or implement a method called getSupportActionBar");
+        }
+        return actionBar;
+    }
+
+    @Override
+    protected int getActionBarHeight() {
+        return mActionBar.getHeight();
+    }
+
+    @Override
+    protected boolean isActionBarNull() {
+        return mActionBar == null;
+    }
+
+    @Override
+    protected void setActionBarBackgroundDrawable(Drawable drawable) {
+        mActionBar.setBackgroundDrawable(drawable);
+    }
+}
