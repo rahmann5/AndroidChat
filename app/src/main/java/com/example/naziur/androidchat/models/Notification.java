d7cb1a7b3465ed45d764b151942c4e67aafa70d1
hsrahman <rahmanh4@aston.ac.uk>
2018-10-01 12:02:52 +0100

    Add new notification design + support for date

--- app/src/main/java/com/example/naziur/androidchat/models/Notification.java
+++ app/src/main/java/com/example/naziur/androidchat/models/Notification.java
@@ -1,5 +1,9 @@
 package com.example.naziur.androidchat.models;
 
+import com.bumptech.glide.annotation.Excludes;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.ServerValue;
+
 /**
  * Created by Hamidur on 14/09/2018.
  */
@@ -8,6 +12,7 @@ public class Notification {
 
     private String sender;
     private String chatKey;
+    private Long date;
 
     public Notification() {
 
@@ -27,5 +32,18 @@ public class Notification {
 
     public void setChatKey(String chatKey) {
         this.chatKey = chatKey;
+    }
+
+    public java.util.Map<String, String> getCreatedDate() {
+        return ServerValue.TIMESTAMP;
+    }
+
+    @Exclude
+    public Long getCreatedDateLong() {
+        return date;
+    }
+
+    public void setCreatedDate(Long createdDate) {
+        this.date = createdDate;
     }
 }
